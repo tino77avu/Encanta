f5ad83756360086aaa240b62bf3d1a9d18b2d12a
INNCode <N_Code_IN@hotmail.com>
2025-12-25 20:54:36 -0500

    Corregir nombre de clase ErrorController para que coincida con el nombre del archivo

--- src/main/java/com/admin/encanta/controller/ErrorController.java
+++ src/main/java/com/admin/encanta/controller/ErrorController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CustomErrorController {
+public class ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
