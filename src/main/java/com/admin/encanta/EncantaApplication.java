fd46e41071ca7db34ed86000cae7fb267c98351e
INNCode <N_Code_IN@hotmail.com>
2025-12-25 21:13:47 -0500

    Corregir ErrorController: renombrar a CustomErrorController, excluir ErrorMvcAutoConfiguration y corregir imports

--- src/main/java/com/admin/encanta/EncantaApplication.java
+++ src/main/java/com/admin/encanta/EncantaApplication.java
@@ -2,8 +2,9 @@ package com.admin.encanta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class EncantaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/admin/encanta/controller/CustomErrorController.java
+++ src/main/java/com/admin/encanta/controller/CustomErrorController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorController {
+public class CustomErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
